d7cf95a97c25e2e289b48d842141250d6e776881
Vuong <vuong@11.com>
2021-10-31 09:42:53 +0700

    add friend

--- CONTACT_API/routes/contact.js
+++ CONTACT_API/routes/contact.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const contacts = require("../controllers/contact");
 
     var router = require("express").Router();
-
+    router.post("/contact", contacts.create)
     router.get("/contact/:from/:to", contacts.checkExist)
     router.get("/contact/:id", contacts.getList)
     router.get("/friend/:userId", contacts.getFriends)
